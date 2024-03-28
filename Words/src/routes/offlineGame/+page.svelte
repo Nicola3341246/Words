d1d81a354953c01720bc4c6aefe3ce374e5f49c3
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2024-03-28 11:41:04 +0100

    Added checker

--- Words/src/routes/offlineGame/+page.svelte
+++ Words/src/routes/offlineGame/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import type { wordGuess, letterGuess } from '$lib';
+	let word: string = '';
+	let guessList: wordGuess[] = [];
+
 	const getWord = async () => {
 		const response = await fetch('/api/wordlist', {
 			method: 'GET',
@@ -6,8 +11,68 @@
 				'content-type': 'application/json'
 			}
 		});
-		console.log(await response.text());
+		word = await response.text();
 	};
+
+	const returnWordGuess = (id: number, letter: string, status: string): letterGuess => {
+		const newletter: letterGuess = {
+			id: id,
+			letter: letter,
+			status: status
+		};
+		return newletter;
+	};
+
+	const checkGuess = async (event: { currentTarget: EventTarget & HTMLFormElement }) => {
+		if (word === '') return;
+
+		const formData = new FormData(event.currentTarget);
+		const newGuess: wordGuess = {
+			letters: []
+		};
+
+		let index = 0;
+		for (let field of formData) {
+			const [key, value] = field;
+			newGuess.letters.push(returnWordGuess(index, value.toString(), 'correct'));
+			index++;
+		}
+
+		console.log(newGuess);
+
+		guessList.push(newGuess);
+		guessList = guessList;
+	};
+
+	if (browser) {
+		getWord();
+	}
 </script>
 
-<button on:click={() => getWord()}>Press</button>
+<div>
+	<div>
+		{#if word === ''}
+			<div>No</div>
+		{:else}
+			{#each guessList as guess}
+				<div class="">
+					<section>{guess.letters[0].letter}</section>
+					<section>{guess.letters[1].letter}</section>
+					<section>{guess.letters[2].letter}</section>
+					<section>{guess.letters[3].letter}</section>
+					<section>{guess.letters[4].letter}</section>
+				</div>
+			{/each}
+			<form action="?/checkGuess" method="post" on:submit|preventDefault={checkGuess}>
+				<div>
+					<input id="input1" name="input" minlength="1" maxlength="1" />
+					<input id="input2" name="input" minlength="1" maxlength="1" />
+					<input id="input3" name="input" minlength="1" maxlength="1" />
+					<input id="input4" name="input" minlength="1" maxlength="1" />
+					<input id="input5" name="input" minlength="1" maxlength="1" />
+				</div>
+				<button>checkGuess</button>
+			</form>
+		{/if}
+	</div>
+</div>
