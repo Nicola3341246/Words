9095f13a76d566c23fe5522c7f77c9584689cd4d
sanqro <contact@sanqro.xyz>
2024-04-27 15:51:37 +0200

    fix duplicate error when saving scores

--- Words/src/routes/scoreboard/+server.ts
+++ Words/src/routes/scoreboard/+server.ts
@@ -4,15 +4,24 @@ import type { IScore } from '$lib/types';
 import { supabaseClient } from '$lib/supabase';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const score: IScore = await request.json();
-    console.log(score);
-    const { error } = await supabaseClient
+    const score: IScore = await request.json();
+    const { data } = await supabaseClient.from<string, IScore | null>("scores").select("*").eq("id", score.id).single();
+    if (data) {
+        const existingScore: IScore = data as IScore;
+        if (existingScore.score >= score.score) {
+            await supabaseClient.from("scores").delete().eq("id", score.id);
+        }
+        if (existingScore.score <= score.score) {
+            return json({ message: "Score not added, because the existing score is higher" }, { status: 200 })
+        }
+    } 
+    const { error: insertError } = await supabaseClient
         .from("scores")
         .insert({ id: score.id, score: score.score, username: score.username, user_uuid: score.user_uuid });
 
-    if (error) {
-        return json({ error: error.message }, { status: 500 });
-    };
+    if (insertError) {
+        return json({ error: insertError.message }, { status: 500 });
+    }
 
     return json({ message: "Score added successfully" });
 }
